84147e2636026d821ae22472a00e404f8efeeb6b
kabir <kabirj2505@gmail.com>
2025-05-31 09:11:56 +0530

    infer

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.data import load_train
-from src.train import train_model
-import torch
-
-train_dataset,train_loader=load_train()
-model=train_model(train_dataset=train_dataset,train_dataloader=train_loader)
-torch.save(model.state_dict(), "models/resnet18_weights.pth")
